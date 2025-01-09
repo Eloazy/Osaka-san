bf56a51e47a6a1a3725ddd83902fc7aaadde7df4
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-09 17:28:42 -0300

    Create pullKey.js

---
+++ system/tornApi/pullKey.js
@@ -0,0 +1,8 @@
+module.exports = async function(key) {
+	var Data = null
+	await fetch(key).then(response => {
+    if(!response.ok) {key_valid = false; return}
+		return response.json()
+	}).then(data => {Data = data})
+	return Data
+}
