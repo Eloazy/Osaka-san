aa787b7f4ac3106c2aa551f10c35dba08db30e17
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-09 17:00:44 -0300

    Create createKey.js

---
+++ system/tornApi/createKey.js
@@ -0,0 +1,9 @@
+const keyParts = [{
+  beforeMacro: "https://api.torn.com/",
+  complement: "/",
+  beforeMicro: "?selections=",
+  key: "&key=",
+  comment: "&comment="
+}]
+
+module.exports = async function(Macro, Micro, UserId) {}
