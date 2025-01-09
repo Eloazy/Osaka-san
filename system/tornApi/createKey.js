7f075bac330caa3b322af65bc54b8bf25091b3bf
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-09 17:24:28 -0300

    Update createKey.js

--- system/tornApi/createKey.js
+++ system/tornApi/createKey.js
@@ -1,14 +1,55 @@
+require('dotenv').config()
+
 const keyParts = [{
   beforeMacro: "https://api.torn.com/",
-  complement: "/",
   beforeMicro: "?selections=",
-  key: "&key=",
-  comment: "&comment="
+  key: "/&key=",
+  comment: "&comment=",
+  keySerial: "null",
 
+  macroOptions: ["user/"],
+  microOptiobs: ["basic/"]
   // with ID
   // https://api.torn.com/MACRO/000000?selections=MICRO&key=KEY
   // without ID
   // https://api.torn.com/MACRO/?selections=basic&key=KEY
 }]
 
-module.exports = async function(Macro, Micro, UserId) {}
+module.exports = async function(Macro, Micro, UserId, reason, complement) {
+  if(complement !== null) {
+    keyParts.key = complement
+  }
+  else {
+    // use process.env
+  }
+  console.log(await createkey(Macro, Micro, UserId))
+}
+
+function createkey(Macro, Micro, UserId, reason) {
+  var key = keyParts.beforeMacro
+  
+  if(UserId == null) {
+    key = key.concat(
+      keyParts.macroOptions[Macro], 
+      UserId,
+      keyParts.beforeMicro, 
+      keyParts.microOptiobs[Micro], 
+      keyParts.key,
+      keyParts.keySerial,
+      keyParts.comnent,
+      reason
+    )
+  }
+  else {
+    key = key.concat(
+      keyParts.macroOptions[Macro], 
+      keyParts.beforeMicro, 
+      keyParts.microOptiobs[Micro], 
+      keyParts.key,
+      keyParts.keySerial,
+      keyParts.comnent,
+      reason
+    )
+  }
+  return key
+}
