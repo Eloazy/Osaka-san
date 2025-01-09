8b7e5775b05c26d75616501cb0d9778112719bfb
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-09 17:05:49 -0300

    Update createKey.js

--- system/tornApi/createKey.js
+++ system/tornApi/createKey.js
@@ -4,6 +4,11 @@ const keyParts = [{
   beforeMicro: "?selections=",
   key: "&key=",
   comment: "&comment="
+
+  // with ID
+  // https://api.torn.com/MACRO/000000?selections=MICRO&key=KEY
+  // without ID
+  // https://api.torn.com/MACRO/?selections=basic&key=KEY
 }]
 
 module.exports = async function(Macro, Micro, UserId) {}
