bc6a6ea027f485c5ed76c512b58b89c0409562d5
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-09 23:24:05 -0300

    Update verify.js

--- system/commands/verify.js
+++ system/commands/verify.js
@@ -1,5 +1,15 @@
 const requestData = require('../tornApi/createKey.js')
 
 module.exports = async function(interaction) {
-  requestData(0, 0, null, "verify", interaction.options.getString("key"))
+  requestData(0, 0, null, "verify", interaction.options.getString("key")).then((data) => {
+    try {
+      
+      await interaction.guild.members.edit(interaction.user.id, {nick: data.name})
+      var vrole = interaction.member.guild.roles.cache.find(role => role.name === "Ember")
+      interaction.member.roles.add(vrole)
+      
+    }catch(error) {
+      console.error("exception in verify.js : ", error)
+    }
+  })
 }
