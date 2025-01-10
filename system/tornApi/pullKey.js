59774a9c288aeafc3c513ed4bb63d62e848f1963
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-09 22:58:12 -0300

    Update pullKey.js

--- system/tornApi/pullKey.js
+++ system/tornApi/pullKey.js
@@ -1,8 +1,8 @@
 module.exports = async function(key) {
-	var Data = null
-	await fetch(key).then(response => {
+    var Data = null
+    await fetch(key).then(response => {
     if(!response.ok) {key_valid = false; return}
-		return response.json()
+	return response.json()
 	}).then(data => {Data = data})
-	return Data
+    return Data
 }
