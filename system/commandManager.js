a2ce4b483da938277d88d52be44eecacab0bb874
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-09 22:57:26 -0300

    Update commandManager.js

--- system/commandManager.js
+++ system/commandManager.js
@@ -3,7 +3,7 @@ const verify = require('./commands/verify.js')
 module.exports = function(interaction) {
   if(interaction.commandName === "verify") {
     verify(interaction).then((response) => {
-      console.log(response)
+      console.log(interaction.user.id+' | run verify command')
     })
   }
 }
