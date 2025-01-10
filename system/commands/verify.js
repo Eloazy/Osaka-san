20f3711650a738294f3e2dc1d46b3d2f0a511ab2
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-10 19:18:06 -0300

    patch correction - role exception

--- system/commands/verify.js
+++ system/commands/verify.js
@@ -3,16 +3,16 @@ const requestData = require('../tornApi/createKey.js')
 module.exports = async function(interaction) {
   requestData(0, 0, null, "verify", interaction.options.getString("key")).then((data) => {
     try {
-      interaction.guild.members.edit(interaction.user.id, {nick: data.name}).catch((error) => {console.error('exception in verify => nickname change')})
+      interaction.guild.members.edit(interaction.user.id, {nick: data.name}).catch((error) => {console.error('exception in verify, permission errors | change name')})
       if(data.faction.faction_id == 53051) {
-        var vrole = interaction.member.guild.roles.cache.find(role => role.name === "Ember").catch((error) => {console.log('Execption in role give : ', error)})
+        var vrole = interaction.member.guild.roles.cache.find(role => role.name === "Ember")
         interaction.member.roles.add(vrole)
         
-        var vposition = interaction.member.guild.roles.cache.find(role => role.name === data.faction.position).catch((error) => {console.log('Execption in role give : ', error)})
+        var vposition = interaction.member.guild.roles.cache.find(role => role.name === data.faction.position)
         interaction.member.roles.add(vposition)
       }
       else {
-        var vrole = interaction.member.guild.roles.cache.find(role => role.name === "visitant").catch((error) => {console.log('Execption in role give : ', error)})
+        var vrole = interaction.member.guild.roles.cache.find(role => role.name === "visitant")
         interaction.member.roles.add(vrole)
       }
       interaction.reply({ content: `thank you ${data.name}, you verifyed yourself, have a good day (serial deleted from our system)`, ephemeral: true})
@@ -20,4 +20,3 @@ module.exports = async function(interaction) {
       console.error("exception in verify.js : ", error)
     }
   })
-}
