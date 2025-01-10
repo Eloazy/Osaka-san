264ae803f99f954f86e534c8dc162f14246de565
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-09 22:51:40 -0300

    Update verify.js

--- system/commands/verify.js
+++ system/commands/verify.js
@@ -1,6 +1,5 @@
 const requestData = require('../tornApi/createKey.js')
 
 module.exports = async function(interaction) {
-  const r =requestData(0, 0, null, "verify", interaction.options.getString("verifyKey"))
-  console.log(r)
+  requestData(0, 0, null, "verify", interaction.options.getString("key"))
 }
