01b47b4911301ad5049c61f1d1683087dd7915f1
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-11 02:47:03 -0300

    Update commandManager.js

--- system/commandManager.js
+++ system/commandManager.js
@@ -1,9 +1,15 @@
 const verify = require('./commands/verify.js')
+const chain = require('./commands/chain.js')
 
 module.exports = function(interaction) {
   if(interaction.commandName === "verify") {
     verify(interaction).then((response) => {
-      console.log(interaction.user.id+' | run verify command')
+      console.log(interaction.user.id+' | run '+interaction.commandName+' command')
+    })
+  }
+  else if(interaction.commandName === "chain") {
+    chain(interaction).then((response) => {
+      console.log(interaction.user.id+' | run '+interaction.commandName+' command')
     })
   }
 }
