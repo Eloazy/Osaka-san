30fe0ea9eada3462d0f0d0747bf131710036c45c
Yume <149901413+Eloazy@users.noreply.github.com>
2025-01-10 21:03:08 -0300

    patch correction - role exception - no idea

--- system/commands/verify.js
+++ system/commands/verify.js
@@ -3,20 +3,24 @@ const requestData = require('../tornApi/createKey.js')
 module.exports = async function(interaction) {
   requestData(0, 0, null, "verify", interaction.options.getString("key")).then((data) => {
     try {
-      interaction.guild.members.edit(interaction.user.id, {nick: data.name}).catch((error) => {console.error('exception in verify, permission errors | change name')})
-      if(data.faction.faction_id == 53051) {
-        var vrole = interaction.member.guild.roles.cache.find(role => role.name === "Ember")
-        interaction.member.roles.add(vrole)
-        
-        var vposition = interaction.member.guild.roles.cache.find(role => role.name === data.faction.position)
-        interaction.member.roles.add(vposition)
+      verifyRole(data)
+      async function verifyRole(data) {
+        interaction.guild.members.edit(interaction.user.id, {nick: data.name}).catch((error) => {console.log('exception in verify, permission errors | change name')})
+        if(data.faction.faction_id == 53051) {
+          var vrole = await interaction.member.guild.roles.cache.find(role => role.name === "Ember")
+          interaction.member.roles.add(vrole)
+          
+          var vposition = await interaction.member.guild.roles.cache.find(role => role.name === data.faction.position)
+          interaction.member.roles.add(vposition)
+        }
+        else {
+          var vrole = await interaction.member.guild.roles.cache.find(role => role.name === "visitant")
+          interaction.member.roles.add(vrole)
+        }
+        interaction.reply({ content: `thank you ${data.name}, you verifyed yourself, have a good day (serial deleted from our system)`, ephemeral: true})
       }
-      else {
-        var vrole = interaction.member.guild.roles.cache.find(role => role.name === "visitant")
-        interaction.member.roles.add(vrole)
-      }
-      interaction.reply({ content: `thank you ${data.name}, you verifyed yourself, have a good day (serial deleted from our system)`, ephemeral: true})
     }catch(error) {
       console.error("exception in verify.js : ", error)
-    }
+    }  
   })
+}
