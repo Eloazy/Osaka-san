0996ec79fec4476cfaee884edc9ec0e0addd89a8
Eloazy <devbombbom@gmail.com>
2025-01-17 01:09:46 -0300

    new command

--- system/commandManager.js
+++ system/commandManager.js
@@ -2,6 +2,7 @@ const verify = require('./commands/verify.js')
 const chain = require('./commands/chain.js')
 const ally = require('./commands/ally.js')
 const enemy = require('./commands/enemy.js')
+const money = require('./commands/money.js')
 
 module.exports = function(interaction) {
   if(interaction.commandName === "verify") {
@@ -29,6 +30,15 @@ module.exports = function(interaction) {
       console.log(interaction.user.id+' | run '+interaction.commandName+' command')
       interaction.reply('watcher stoped')
     })
+  }
+  else if(interaction.options._subcommand == "withdraw") {
+    enemy(interaction, null, true).then((response) => {
+      console.log(interaction.user.id+' | run '+interaction.commandName+' command')
+      interaction.reply('watcher stoped')
+    })
+  }
+  else if(interaction.options._subcommand == "loan") {
+    interaction.reply('developing')
   }
   else {interaction.reply('this command has not registered or you dont have the perms to use')}
 }
---
+++ system/commands/money.js
@@ -0,0 +1,7 @@
+module.exports = async function(interaction) {
+	
+}
+
+function requestMoney(interaction) {
+	
+}
